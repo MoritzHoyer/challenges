765108f597100a286100e772dd1fad1adce4a0e5
Moritz Hoyer <contact@moritzhoyer.de>
2024-08-01 12:08:58 +0200

    added tip-calculator

---
+++ js-variables-and-numbers/tip-calculator/js/index.js
@@ -0,0 +1,8 @@
+let mealCost = 25.0;
+const tipPercentage = 0.1;
+
+const tipAmount = mealCost * tipPercentage;
+
+const totalCost = mealCost + tipAmount;
+
+console.log(`The total cost of your bill is: ${totalCost} euros`);
