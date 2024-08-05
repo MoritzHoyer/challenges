e7f9b410cbc11ac71cc41e6fa2d5849d0dbdd94a
Moritz Hoyer <contact@moritzhoyer.de>
2024-08-05 09:31:13 +0200

    changes

--- js-variables-and-numbers/js-variables-and-numbers_track-your-savings-goals/js/index.js
+++ js-variables-and-numbers/js-variables-and-numbers_track-your-savings-goals/js/index.js
@@ -1,1 +1,14 @@
+const startingBalance = 1000;
+const monthlyContribution = 200;
 
+const months = 12;
+
+const totalSavings = startingBalance + monthlyContribution * months;
+
+console.log(
+  "After " +
+    months +
+    " months of saving, you will have $" +
+    totalSavings +
+    " in your account."
+);
