f1469c7f07f44c3b42090bfaf1d2ab72c4994de1
Moritz Hoyer <contact@moritzhoyer.de>
2024-08-07 17:11:35 +0200

    completed handle-form-submit

--- js-forms/js-forms_handle-form-submit/js/index.js
+++ js-forms/js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,17 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  //   console.log("hello");
+
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+
+  const firstName = data.firstName;
+  const ageBadnessSum = Number(data.badness) + Number(data.age);
+
+  console.log(data);
+  console.log(`The age-badness-sum of ${firstName} is ${ageBadnessSum}`);
+});
