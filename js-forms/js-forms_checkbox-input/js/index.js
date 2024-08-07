a91e4f39b06e87e3502c8ddaf1189e40fa142206
Moritz Hoyer <contact@moritzhoyer.de>
2024-08-07 18:51:55 +0200

    completed checkbox-input

--- js-forms/js-forms_checkbox-input/js/index.js
+++ js-forms/js-forms_checkbox-input/js/index.js
@@ -3,6 +3,7 @@ console.clear();
 const form = document.querySelector('[data-js="form"]');
 const tosError = document.querySelector('[data-js="tos-error"]');
 const tosCheckbox = document.querySelector('[data-js="tos"]');
+const successMessage = document.querySelector('[data-js="success"]');
 
 function hideTosError() {
   tosError.setAttribute("hidden", "");
@@ -12,13 +13,36 @@ function showTosError() {
   tosError.removeAttribute("hidden");
 }
 
+hideTosError();
+successMessage.setAttribute("hidden", "");
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   // --v-- write your code here --v--
 
+  if (!tosCheckbox.checked) {
+    showTosError();
+    return;
+  } else {
+    hideTosError();
+  }
+
+  successMessage.removeAttribute("hidden");
+
   // --^-- write your code here --^--
 
   // eslint-disable-next-line no-alert
   alert("Form submitted");
+
+  form.reset();
+  document.getElementById("first-name").focus();
+});
+
+tosCheckbox.addEventListener("change", () => {
+  if (tosCheckbox.checked) {
+    hideTosError();
+  } else {
+    showTosError();
+  }
 });
