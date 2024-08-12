3d1c210e37e7d7b3874d061cc816a1a30c874b28
Moritz Hoyer <contact@moritzhoyer.de>
2024-08-12 17:43:56 +0200

    completed stars

--- js-loops/js-loops_stars/js/index.js
+++ js-loops/js-loops_stars/js/index.js
@@ -2,13 +2,46 @@ console.clear();
 
 const starContainer = document.querySelector('[data-js="star-container"]');
 
-function renderStars() {
+function renderStars(filledStars) {
   // reset the star container before rerendering stars
+
   starContainer.innerHTML = "";
 
   //--v-- your code here --v--
 
+  // // 1.
+  // for (let i = 1; i <= 5; i++) {
+  //   const star = document.createElement("img");
+  //   star.src = "assets/star-empty.svg";
+  //   starContainer.append(star)
+  // }
+
+  // 2.
+  //   for (let i = 1; i <= 5; i++) {
+  //     const star = document.createElement("img");
+
+  //     if (i <= filledStars) {
+  //       star.src = "assets/star-filled.svg";
+  //     } else {
+  //       star.src = "assets/star-empty.svg";
+  //     starContainer.append(star);
+  //   }
+  // }
+
+  //3.
+  for (let i = 1; i <= 5; i++) {
+    const star = document.createElement("img");
+    star.src =
+      i <= filledStars ? "assets/star-filled.svg" : "assets/star-empty.svg";
+
+    star.addEventListener("click", () => {
+      renderStars(i);
+    });
+
+    starContainer.append(star);
+  }
+
   //--^-- your code here --^--
 }
 
-renderStars();
+renderStars(0);
