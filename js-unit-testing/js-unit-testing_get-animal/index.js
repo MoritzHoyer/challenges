7af3f1225bfa455fc676f58a57b6aa5dd7a4783b
Moritz Hoyer <contact@moritzhoyer.de>
2024-08-20 11:19:39 +0200

    completed get-animal

--- js-unit-testing/js-unit-testing_get-animal/index.js
+++ js-unit-testing/js-unit-testing_get-animal/index.js
@@ -1,1 +1,11 @@
-export function getAnimal() {}
+export function getAnimal(animals) {
+  if (animals === "cats") {
+    return "i totally love cats";
+  }
+
+  if (animals) {
+    return `I like ${animals}!`;
+  }
+
+  return "i do not like animals at all";
+}
