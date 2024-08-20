3c8076163121239b4de32d5d857175941b5cb2bc
Moritz Hoyer <contact@moritzhoyer.de>
2024-08-20 12:06:13 +0200

    fixed typo mistakes

--- js-unit-testing/js-unit-testing_get-animal/index.js
+++ js-unit-testing/js-unit-testing_get-animal/index.js
@@ -1,11 +1,11 @@
 export function getAnimal(animals) {
   if (animals === "cats") {
-    return "i totally love cats";
+    return "I totally love cats!";
   }
 
   if (animals) {
     return `I like ${animals}!`;
   }
 
-  return "i do not like animals at all";
+  return "I do not like animals at all!";
 }
