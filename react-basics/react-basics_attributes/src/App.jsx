819abb65a9ded7f5fefdb2433199909bd5027019
Moritz Hoyer <contact@moritzhoyer.de>
2024-08-23 15:49:01 +0200

    completed attributes

--- react-basics/react-basics_attributes/src/App.jsx
+++ react-basics/react-basics_attributes/src/App.jsx
@@ -1,5 +1,15 @@
 import "./App.css";
 
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return (
+    <article className="article">
+      <h2 className="articel__title">Here we react again</h2>
+      <label htmlFor="articleInput">Type here:</label>
+      <input id="articleInput" type="text" />
+      <a
+        className="article__link"
+        href="https://de.wikipedia.org/wiki/Bootcamp"
+      ></a>
+    </article>
+  );
 }
