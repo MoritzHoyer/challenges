2209ca9305e73c9550018eefc49ed563c9dc82ad
Moritz Hoyer <contact@moritzhoyer.de>
2024-08-30 13:15:26 +0200

    complete pokemon-api-with-paging

--- react-effects-and-fetch/react-effects-and-fetch_pokemon-api-with-paging/src/components/PokemonList.jsx
+++ react-effects-and-fetch/react-effects-and-fetch_pokemon-api-with-paging/src/components/PokemonList.jsx
@@ -1,15 +1,15 @@
 import { useEffect, useState } from "react";
 import "./PokemonList.css";
 
-
 export default function PokemonList() {
   const [pokemon, setPokemon] = useState([]);
+  const [offset, setOffset] = useState(0);
 
   useEffect(() => {
     async function loadPokemon() {
       try {
         const response = await fetch(
-          "https://pokeapi.co/api/v2/pokemon?offset=0"
+          `https://pokeapi.co/api/v2/pokemon?offset=${offset}`
         );
         const data = await response.json();
         setPokemon(data.results);
@@ -19,15 +19,36 @@ export default function PokemonList() {
     }
 
     loadPokemon();
-  }, []);
+  }, [offset]);
+
+  // Handler for "Next Page" button click. Move to the next page
+  const handleNextPage = () => {
+    setOffset((prevOffset) => prevOffset + 20);
+  };
+
+  // Handler for "Previous Page" button click. Move to the previous page, not below 0
+  const handlePreviousPage = () => {
+    setOffset((prevOffset) => Math.max(prevOffset - 20, 0));
+  };
 
   return (
     <main>
-      <button type="button" className="button">Previous Page</button>
-      <button type="button" className="button">Next Page</button>
+      <button
+        type="button"
+        className="button"
+        onClick={handlePreviousPage}
+        disabled={offset === 0}
+      >
+        Previous Page
+      </button>
+      <button type="button" className="button" onClick={handleNextPage}>
+        Next Page
+      </button>
       <ul>
         {pokemon.map(({ name }) => (
-          <li key={name} className="pokemon">{name}</li>
+          <li key={name} className="pokemon">
+            {name}
+          </li>
         ))}
       </ul>
     </main>
